9e661c169eeac4416f2ac44e0d3d68084fd385ea
misha_sh <misha_she@mail.ru>
2020-05-05 19:54:51 +0300

    task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Threading;
 
 /* В задаче не использовать циклы for, while. Все действия по обработке данных выполнять с использованием LINQ
  * 
@@ -27,41 +30,67 @@ using System.Linq;
  * В случае возникновения иных нештатных ситуаций (например, в случае попытки итерирования по пустой коллекции) 
  * выбрасывайте InvalidOperationException!
  */
-namespace Task02
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Task02 {
+    class Program {
+        static void Main(string[] args) {
             RunTesk02();
         }
 
-        public static void RunTesk02()
-        {
+        public static void RunTesk02() {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("ru");
+
             int[] arr;
-            try
-            {
+            try {
                 // Попробуйте осуществить считывание целочисленного массива, записав это ОДНИМ ВЫРАЖЕНИЕМ.
-                arr = 
+                arr = Console.ReadLine()
+                    .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                if (arr.Length == 0)
+                    throw new InvalidOperationException();
             }
-            
-            
-            var filteredCollection = arr.
-           
-            try
-            {
-                
+            catch (FormatException ex) {
+                Console.WriteLine("FormatException");
+                return;
+            }
+            catch (IOException ex) {
+                Console.WriteLine("IOException");
+                return;
+            }
+            catch (InvalidOperationException ex) {
+                Console.WriteLine("InvalidOperationException");
+                return;
+            }
+            catch (OverflowException ex) {
+                Console.WriteLine("OverflowException");
+                return;
+            }
+
+
+            var filteredCollection = arr.TakeWhile(num => num != 0).ToList();
+
+            try {
+
                 // использовать статическую форму вызова метода подсчета среднего
-                double averageUsingStaticForm = 
+                double averageUsingStaticForm = Enumerable.Average(filteredCollection.Select(x => x*x));
                 // использовать объектную форму вызова метода подсчета среднего
-                double averageUsingInstanceForm = 
+                double averageUsingInstanceForm = filteredCollection.Select(x => x*x).Average();
 
+                Console.WriteLine($"{averageUsingStaticForm:f3}".Replace('.', ','));
+                Console.WriteLine($"{averageUsingInstanceForm:f3}".Replace('.', ','));
 
-                // вывести элементы коллекции в одну строку
-                filteredCollection.
+                // вывести элементы коллекции в одну строку 
+                Console.WriteLine(
+                    filteredCollection.Select(t => t.ToString())
+                        .Aggregate((elem1, elem2) => $"{elem1} {elem2}"));
             }
-          
+            catch (OverflowException) {
+                Console.WriteLine("OverflowException");
+            }
+            catch (InvalidOperationException) {
+                Console.WriteLine("InvalidOperationException");
+            }
         }
-        
+
     }
 }
